ff049f8cd751e681527f46c3930276ca4cde603e
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2022-11-19 16:37:43 +0800

    Init Project

---
+++ lib/calculator.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Calculator extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        
+      ),
+    );
+  }
+}
