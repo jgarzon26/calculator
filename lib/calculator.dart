9a3774591a18465c3e40b43a23ebe4c508ce19ed
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2022-11-22 10:51:55 +0800

    makeSomeFuncPrivateAndDecimalImplement

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -24,16 +24,17 @@ class _CalculatorState extends State<Calculator> {
   final _userInput = TextEditingController(text: "0");
   final _result = TextEditingController();
 
-  void addNumberInInputField(int num) {
+  void _addNumberInInputField(int num) {
     setState(() => _checkIfInputFieldHasValue()
         ? _userInput.text = num.toString()
         : _userInput.text += num.toString());
   }
-  void addOperatorInInputField(String oper) {
+  void _addOperatorInInputField(String oper) {
     setState(() => _userInput.text += oper);
   }
 
-  bool _checkIfInputFieldHasValue() => _userInput.text.length == 1 && _userInput.text.startsWith("0") ? true : false;
+  bool _checkIfInputFieldHasValue() => _userInput.text.length == 1
+      && _userInput.text.startsWith("0") ? true : false;
 
   void _getResultFromString(){
     var expression = _userInput.text;
@@ -69,91 +70,89 @@ class _CalculatorState extends State<Calculator> {
         appBar: AppBar(
           title: Text("Calculator",),
         ),
-        body: Form(
-          child: Column(
-            children: [
-              TextFormField(
-                controller: _userInput,
-                keyboardType: TextInputType.none,
-                textAlign: TextAlign.right,
-              ),
-              TextField(
-                controller: _result,
-                keyboardType: TextInputType.none,
-                textAlign: TextAlign.right,
-                readOnly: true,
-              ),
-              IconButton(
+        body: Column(
+          children: [
+            TextFormField(
+              controller: _userInput,
+              keyboardType: TextInputType.none,
+              textAlign: TextAlign.right,
+            ),
+            TextField(
+              controller: _result,
+              keyboardType: TextInputType.none,
+              textAlign: TextAlign.right,
+              readOnly: true,
+            ),
+            IconButton(
+                onPressed: null,
+                icon: Icon(
+                  Icons.backspace_outlined,
+                ),
+            ),
+            Row(
+              children: [
+                ElevatedButton(
+                  onPressed: () => setState(() {
+                    _userInput.text = "0";
+                    _result.clear();
+                  }),
+                  child: Text("C",),
+                ),
+                ElevatedButton(
+                    onPressed: null,
+                    child: Text("( )"),
+                ),
+                ElevatedButton(
                   onPressed: null,
-                  icon: Icon(
-                    Icons.backspace_outlined,
-                  ),
-              ),
-              Row(
-                children: [
-                  ElevatedButton(
-                    onPressed: () => setState(() {
-                      _userInput.text = "0";
-                      _result.clear();
-                    }),
-                    child: Text("C",),
-                  ),
-                  ElevatedButton(
-                      onPressed: null,
-                      child: Text("( )"),
-                  ),
-                  ElevatedButton(
-                    onPressed: null,
-                    child: Text("%"),
-                  ),
-                  OperButton(_listOfOperators['divide']!, addOperatorInInputField),
-                ],
-              ),
-              Row(
-                children: [
-                  NumberButton(7, addNumberInInputField),
-                  NumberButton(8, addNumberInInputField),
-                  NumberButton(9, addNumberInInputField),
-                  OperButton(_listOfOperators['multiply']!, addOperatorInInputField),
-                ],
-              ),
-              Row(
-                children: [
-                  NumberButton(4, addNumberInInputField),
-                  NumberButton(5, addNumberInInputField),
-                  NumberButton(6, addNumberInInputField),
-                  OperButton(_listOfOperators['subtract']!, addOperatorInInputField),
-                ],
-              ),
-              Row(
-                children: [
-                  NumberButton(1, addNumberInInputField),
-                  NumberButton(2, addNumberInInputField),
-                  NumberButton(3, addNumberInInputField),
-                  OperButton(_listOfOperators['add']!, addOperatorInInputField),
-                ],
-              ),
-              Row(
-                children: [
-                  ElevatedButton(
-                    onPressed: null,
-                    child: Text("+/-"),
-                  ),
-                  NumberButton(0, addNumberInInputField),
-                  ElevatedButton(
-                      onPressed: null,
-                      child: Text(".")
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      _getResultFromString();
-                    },
-                    child: Text("=",),
-                  ),
-                ],
-              )
-            ],
-          ),
+                  child: Text("%"),
+                ),
+                OperButton(_listOfOperators['divide']!, _addOperatorInInputField),
+              ],
+            ),
+            Row(
+              children: [
+                NumberButton(7, _addNumberInInputField),
+                NumberButton(8, _addNumberInInputField),
+                NumberButton(9, _addNumberInInputField),
+                OperButton(_listOfOperators['multiply']!, _addOperatorInInputField),
+              ],
+            ),
+            Row(
+              children: [
+                NumberButton(4, _addNumberInInputField),
+                NumberButton(5, _addNumberInInputField),
+                NumberButton(6, _addNumberInInputField),
+                OperButton(_listOfOperators['subtract']!, _addOperatorInInputField),
+              ],
+            ),
+            Row(
+              children: [
+                NumberButton(1, _addNumberInInputField),
+                NumberButton(2, _addNumberInInputField),
+                NumberButton(3, _addNumberInInputField),
+                OperButton(_listOfOperators['add']!, _addOperatorInInputField),
+              ],
+            ),
+            Row(
+              children: [
+                ElevatedButton(
+                  onPressed: null,
+                  child: Text("+/-"),
+                ),
+                NumberButton(0, _addNumberInInputField),
+                ElevatedButton(
+                    onPressed: () => _addOperatorInInputField("."),
+                    child: Text(".")
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    _getResultFromString();
+                  },
+                  child: Text("=",),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
